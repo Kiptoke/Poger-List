a629ef879c2fefed30ff325697e2f93361dda830
Andrew Zhou <kiptoke@gmail.com>
2025-02-23 23:43:50 -0500

    Python backend restored

---
+++ python/constants.py
@@ -0,0 +1,68 @@
+USER_TABLE = {
+    'h9wxnd3xvrfdfjv7r9p3ihz68':    'kaavya', 
+    '315qfzr3gmd5m4mkjoprzwsny7cq': 'musa', 
+    '31fhaf5kxu7r4p3fr5r2klb5d23a': 'andrew', 
+    '31d6s5qq4uxpm6ucmue2p3ykb37q': 'natasha', 
+    '31giifsv5jaavljfciuappzifs3u': 'naveen', 
+    'blackupblackup5':              'aman', 
+    'kush_p':                       'kushal', 
+    'je1mzixeidgq3qxp5v10r4iih':    'faiyaz', 
+    'rennylop':                     'renny',
+    'freezercune':                  'billy',
+    'lwbl3eraki1c231y7yp06f7mn':    'val',
+    'vyfxzshzsqdekexbl6h6ztqoh':    'dilan'
+}
+
+PLAYLISTS = [
+    #"3qIM3KhUdSWBPfATq96DrT", # melody is poger - unsupported right now
+    "5OvfL5wNwDXbhwxViVqEOH", # rhythmy is poger
+    "1wxAV5qzcR9UmlcBkDRQI7", # poger 4
+    "3aJiDadyEFDsZScRsvmfiV", # music is poger
+    "2kIKZnYdfdTpmJ2mdoYM6a", # pogcraft
+    "4rvTKzFa5vDTyOpqORLftY", # pogget
+    "1kMOhUERX0YoCvl35oQNbF", # jpoger
+    "39PdNwabAaSI05SH7CR2gk", # instrupoger
+    "5HH6tfzuNxzKPFvwBvKg0S", # pogshal
+    "0Tc4OQlFeQ6VdF3CXi0UmT", # pogsmall
+    "0zCJ8zPnbQVqdzqa7w3yyO", # pogshort
+    "2GH4em9cP0J1WRq16yXOXT", # puker
+    "4hQKG5p8PmoEEc7ulj0Bd1", # normal poger
+    "2hobytgmKfvnPb71WT9p91", # cultoger
+    "7qmgxBZPPZnXWqNGubnm9f", # gamer is goger
+    "1YYxdcnsydRkaaAstTNq3V", # musoger is here
+    "7cvH77lDwWeNUnYTWZ4uvs", # psychic damoger
+    "6CXzw15tLrVdi8Lj5hIHXJ", # piazzoger
+    "5dfgNeR2MP9GpBVDMVBMXy", # pogern't
+    "42AfPKy2wlP92C0vmF53Td", # pogers back baybee
+    "3zRSkCIHCBjKtkepjUChns", # poger 4 2
+    "19DItAT61EH7w8wtLNvp9d", # femoger 
+    "3BbJSSxPrwBy11E3RMcBcn", # jesse we need to poge
+    "0LuMvyk7BP69xZFIl548yl", # poger 4 3
+    "6T147mocWVYgqE4BKKdJt9", # pawj
+    "5ZTBo67Q07MXxEBr9aO4tS", # usoger
+    "3jvG2hlBqATYxU4Zbzs8iM", # kroger
+    "0Rhoi7w0ay1noEPrxfNEpT", # normoger
+    "6cHuHc6jEHh8Q9A8pAoK7F", # pogirl
+    "5gFFcrylmPZUyNbA823zFR", # rat poger
+    "5zTKvMThkCOAfAmqEhbuf3", # rapoger
+    "1VkjKQ0yrFPp9dsqbVxEbB", # street poger 6
+    "0ybDZQ6lC572kgwuwRLUvo", # minioger
+    "0sSTCIWqrxHdJUc36UUvUH", # smells like teen poger
+    "2bEvJv3izBlFyv4hide2y2", # smoger
+    "1p1RwpqBO2cHO1s0hVXyVe", # abnormoger
+    "4lEpkO1w6FDfneoHFGNWzK", # boger
+    "2pbeeQCbpMWS2ndBCNbMh3", # cultwoger
+    "7F8Ic0QGRMtstKXl6r9teT", # austin yoger
+    "2Ey0mtd3hv1PtgfL2dqXdz", # we didn't start the poger
+    "7jtOfjlMmT9mY0DJ1NGTRK", # dragoger boger
+    "4ygWEYXQFSaUl6lnGMj9kc", # the answoger
+    "2Y6yJhX8jEg2JUPufNsANj", # potatoger
+    "6T0IFLhdKazRkB9KcGfXGa", # abnormoger ii
+    "6HPPvmBVmsrcAer8YsNEUj", # summoger
+    "2xqu65lcuyfnW91fPfZyy6", # pogelore
+    "5lqBn9OMNAv7gD1JkOtQCN", # no man's skoger
+    "35M7vsdd0u0vHR0amRfc4n", # return of the poge
+    "5fX27gyTmOg04efPO3l0Po", # contropoger
+    "0v6QlVZFVwLHJd9SQsxOWc", # jerkoger
+    "0fSaha56OvVsJgfVpm7Ej0", # smoger 2
+]
